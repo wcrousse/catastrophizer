442c1766994320b2f040cc21bbc2587670b6cfc4
William Rousse <wrousse@incomm.com>
2021-12-09 06:37:55 -0800

    copy in demo project

---
+++ NotifyPropertyChangedLiveDemo/CarModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel;
+
+namespace NotifyPropertyChangedLiveDemo
+{
+  public partial class CarModel : INotifyPropertyChanged
+  {
+    private double SpeedKmPerHourBackingField;
+    private int NumberOfDoorsBackingField;
+    private string ModelBackingField = "";
+
+    public void SpeedUp() => SpeedKmPerHour *= 1.1;
+  }
+}
